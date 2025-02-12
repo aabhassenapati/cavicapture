2025221afc415d6ceceab5bb61d1b10ce41eaddf
Aabhas Senapati <aabhas2211@gmail.com>
2025-02-12 10:34:01 -0800

    Updates for Code

--- seq_converter.py
+++ seq_converter.py
@@ -1,119 +1,114 @@
-#!/usr/bin/python
-
-from ConfigParser import SafeConfigParser
+import configparser
 import sqlite3
-import sys, os, getopt
-import time, datetime
+import sys
+import os
+import getopt
+import time
+import datetime
 import glob
-import string
-from datetime import datetime
 from shutil import copyfile
 
-def main():
 
-    input_directory = ''
-    output_directory = ''
-    file_mask = ''
-    sequence_name = ''
+def main():
+    input_directory = ""
+    output_directory = ""
+    file_mask = ""
+    sequence_name = ""
+    config_path = ""
 
     try:
-      opts, args = getopt.getopt(sys.argv[1:], "c:i:o:s:f:", ["config=", "input_dir=", "output_dir=", "sequence_name=", "file_mask="])
+        opts, args = getopt.getopt(sys.argv[1:], "c:i:o:s:f:",
+                                   ["config=", "input_dir=", "output_dir=", "sequence_name=", "file_mask="])
     except getopt.GetoptError:
-      print("seq_converter.py --config <path/to/config.ini> --input_dir <path/to/capture/directory> --output_dir <path/to/new/directory --sequence_name= <name> --file_mask <mask>")
-      sys.exit(2)
-    
+        print("Usage: seq_converter.py --config <path/to/config.ini> --input_dir <path/to/capture/directory> "
+              "--output_dir <path/to/new/directory> --sequence_name <name> --file_mask <mask>")
+        sys.exit(2)
+
     for opt, arg in opts:
-      if opt in ("--config"):
-        config_path = arg
-      elif opt in ("--input_dir"):
-        input_directory = arg
-      elif opt in ("--output_dir"):
-        output_directory = arg
-      elif opt in ("--file_mask"):
-        file_mask = arg
-      elif opt in ("--sequence_name"):
-        sequence_name = arg
-      
+        if opt == "--config":
+            config_path = arg
+        elif opt == "--input_dir":
+            input_directory = arg
+        elif opt == "--output_dir":
+            output_directory = arg
+        elif opt == "--file_mask":
+            file_mask = arg
+        elif opt == "--sequence_name":
+            sequence_name = arg
+
     cavi_converter = CaviConverter(config_path, input_directory, output_directory, file_mask, sequence_name)
     cavi_converter.init()
 
-class CaviConverter:      
 
-  def __init__(self, config_file, input_directory, output_directory, file_mask, sequence_name):
-      
-    self.config_file = config_file  
-    self.input_directory = input_directory  
-    self.output_directory = output_directory  
-    self.file_mask = file_mask  
-    self.sequence_name = sequence_name  
+class CaviConverter:
+    def __init__(self, config_file, input_directory, output_directory, file_mask, sequence_name):
+        self.config_file = config_file
+        self.input_directory = input_directory
+        self.output_directory = output_directory
+        self.file_mask = file_mask
+        self.sequence_name = sequence_name
 
-    self.setup_directories()
-    self.setup_db()
-    self.load_config()
+        self.setup_directories()
+        self.setup_db()
+        self.load_config()
 
-  def init(self):
+    def init(self):
+        sql = "DELETE FROM captures;"
+        self.db_conn.execute(sql)
+        self.db_conn.commit()
+        self.find_captures()
+        self.write_config()
 
-    # Clear the new db
-    sql = "DELETE FROM captures;"
-    self.db_conn.execute(sql)
-    self.db_conn.commit()
-    self.find_captures()
-    self.write_config()
+    def write_config(self):
+        self.config.set("capture", "output_dir", self.output_directory.rstrip("/"))
+        self.config.set("capture", "sequence_name", self.sequence_name)
 
-  def write_config(self):
+        with open(os.path.join(self.output_sequence_path, "config.ini"), "w") as config_file:
+            self.config.write(config_file)
 
-    self.config.set('capture', 'output_dir', self.output_directory.rstrip("/"));
-    self.config.set('capture', 'sequence_name', self.sequence_name);
+    def find_captures(self):
+        for file_path in glob.glob(os.path.join(self.input_directory, self.file_mask)):
+            print(f"Processing file: {file_path}")
+            file_name = os.path.basename(file_path)
+            name, _ = os.path.splitext(file_name)
+            try:
+                file_time = datetime.datetime.strptime(name, "%Y%m%d-%H%M%S")
+            except ValueError:
+                print(f"Skipping file {file_name}, invalid timestamp format.")
+                continue
 
-    with open(self.output_sequence_path + "/config.ini", 'w') as config_file:
-      self.config.write(config_file)    
+            sql = "INSERT INTO captures (filename, timestamp, skip, processing, processed) VALUES (?, ?, 0, 0, 0);"
+            self.db_conn.execute(sql, (file_name, file_time.strftime("%Y%m%d-%H%M%S")))
+            self.db_conn.commit()
+            copyfile(file_path, os.path.join(self.output_sequence_path, file_name))
 
-  def find_captures(self):
+    def setup_directories(self):
+        self.input_directory = os.path.abspath(self.input_directory)
+        self.output_directory = os.path.abspath(self.output_directory)
+        self.output_sequence_path = os.path.join(self.output_directory, self.sequence_name)
 
-    # print(glob.glob("/home/adam/*.txt"))
-    for file_path in glob.glob(self.input_directory + self.file_mask):
-      print "File " + file_path + "(" + os.path.basename(file_path) + ")"
-      file_name = os.path.basename(file_path)
-      name = os.path.basename(file_path).replace(".png", "")
-      file_time = datetime.strptime(name, '%Y%m%d-%H%M%S')
-      print file_time.strftime('%Y%m%d-%H%M%S')
-      sql = "INSERT INTO captures (filename, timestamp, skip, processing, processed) VALUES ('" + file_name + "', '" + file_time.strftime('%Y%m%d-%H%M%S') + "', 0, 0, 0);"
-      self.db_conn.execute(sql)
-      self.db_conn.commit()
-      copyfile(file_path,  self.output_sequence_path + "/" + file_name)
+        os.makedirs(self.output_directory, exist_ok=True)
+        os.makedirs(self.output_sequence_path, exist_ok=True)
+
+    def load_config(self):
+        self.config = configparser.ConfigParser()
+        self.config.read(self.config_file)
+
+    def setup_db(self):
+        self.db_conn = sqlite3.connect(os.path.join(self.output_sequence_path, "capture.db"))
+        self.db_conn.execute("""
+            CREATE TABLE IF NOT EXISTS captures (
+                id INTEGER PRIMARY KEY,
+                filename TEXT NOT NULL,
+                timestamp TEXT NOT NULL,
+                skip INTEGER NOT NULL,
+                processed INTEGER NOT NULL,
+                processing INTEGER NOT NULL,
+                area REAL
+            );
+        """)
+        self.db_conn.commit()
 
 
-  def setup_directories(self):
-    
-    if not self.input_directory.endswith("/"):
-      self.input_directory = self.input_directory + "/"
-    
-    if not self.output_directory.endswith("/"):
-      self.output_directory = self.output_directory + "/"
-
-    self.output_sequence_path = self.output_directory + self.sequence_name     
-
-    if not os.path.exists(self.output_directory):
-      os.makedirs(self.output_directory)
-
-    if not os.path.exists(self.output_sequence_path):
-      os.makedirs(self.output_sequence_path)
-
-  def load_config(self):
-
-    self.config = SafeConfigParser()
-    self.config.read(self.config_file)
-
-  def setup_db(self):
-    self.db_conn = sqlite3.connect(self.output_sequence_path + '/capture.db')
-    self.db_conn.execute('''CREATE TABLE IF NOT EXISTS captures
-            (id INTEGER PRIMARY KEY,
-            filename CHAR(50) NOT NULL,
-            timestamp CHAR(50) NOT NULL,
-            skip INT NOT NULL,
-            processed INT NOT NULL,
-            processing INT NOT NULL,
-            area REAL);''')
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
